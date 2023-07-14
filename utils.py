505d8a1674017f3bde9c5fed1744bb962f4a9484
Angelika V <K12227403@students.jku.at>
2023-07-14 10:42:45 +0200

    added description of plot_losses function

--- utils.py
+++ utils.py
@@ -7,7 +7,13 @@ Utils file of Image Depixelation Project.
 """
 from matplotlib import pyplot as plt
 
-def plot_loss(train_losses, eval_losses, eval_epochs):
+def plot_loss(train_losses: list, eval_losses: list, eval_epochs: list):
+    """ Function for plotting training and evaluation loss.
+
+    :param train_losses: list of training losses
+    :param eval_losses: list of evaluation losses
+    :param eval_epochs: list of epochs at which evaluation was performed
+    """
     x = range(len(train_losses))
 
     plt.plot(x, train_losses, label='train loss')
